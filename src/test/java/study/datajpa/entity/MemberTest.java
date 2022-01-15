5db69dccc78e8894c1c54bf51d183ae3392326f4
sugil <privatesugil@gmail.com>
2022-01-15 15:42:45 +0900

    Persist 혹은 Update 전/후 에 값 적용 실습 - 등록일, 수정일, 등록자, 수정자등 테이블에서 공통으로 사용하는 속성을 순수 JPA를 이용해서 관리하는 방법에 대해 실습하였다.

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,11 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,6 +21,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() throws Exception {
@@ -49,4 +54,24 @@ class MemberTest {
             System.out.println("-> member.team = " + member.getTeam());
         }
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist
+
+        Thread.sleep(100); //실제 테스트에는 이런 sleep를 넣으면 안됨. updatedDate가 제대로 적용됐는지 확인하기 위해 넣었음 여기서는...
+        member.setUsername("member2");
+
+        em.flush(); //@PreUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+
+     }
 }
