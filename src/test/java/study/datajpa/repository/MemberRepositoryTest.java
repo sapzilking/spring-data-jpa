f0a7a4324577aecdac3e65c2afe91cb3f4134972
sugil <privatesugil@gmail.com>
2022-01-15 13:38:49 +0900

    1. JPA Hint에 대해 공부   - Realonly로 하면 스냅샷도 생기지 않고 나름 최적화가 되긴 하지만, 개발 초기부터 readonly인 곳에 이걸 다 넣으면서 하기보다는 없이 하다가 성능이슈가 생기면 그부분만 해결하던지 redis같은 캐시를 이용하던지.. 2. JPA에서 제공해 주는 lock의 간단한 사용법에 대해 공부

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -371,7 +371,30 @@ class MemberRepositoryTest {
             //then
          }
 
+         @Test
+         public void queryHint() throws Exception {
+             //given
+             Member member1 = memberRepository.save(new Member("member1", 10));
+             em.flush();
+             em.clear();
 
+             //when
+             Member findMember = memberRepository.findReadOnlyByUsername("member1");
+             findMember.setUsername("member2");
+
+             em.flush();
+         }
+
+         @Test
+         public void lock() throws Exception {
+             //given
+             Member member1 = memberRepository.save(new Member("member1", 10));
+             em.flush();
+             em.clear();
+
+             //when
+             List<Member> result = memberRepository.findLockByUsername("member1");
+         }
 
 
 
